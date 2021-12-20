23ce32d4d26e3c2111ba018fe0cb9e446b546774
Mukhar Jain <Mukhar@users.noreply.github.com>
2021-12-20 23:45:58 +0530

    Update footer enhancements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,34 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import "./../assets/css/footer.css"
-import gun from './../assets/icons/Gunayatan_Black.png'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+// import gun from './../assets/icons/Gunayatan_Black.png'
+import gun from './../assets/icons/Gunayatan_Grey.svg'
+import logo from "./../assets/images/gunayatan_logo.png"
+
 export default function Footer() {
     return (<>
-        <Row ClassName="justify-content-center">
+        <Row style={{ justifyContent: "center" }}>
             <img src={gun} style={{ maxWidth: "200px" }} ></img>
         </Row>
-        <Container fluid className='footer'>
+        <Container fluid className='footer py-2' >
             <Container>
-                <Row className=' row justify-content-between'>
-                    <Col>GUNAYATAN LOGO</Col>
-                    <Col>Search</Col></Row>
-                <Row lg={5} xs={2}>
+                <Row className='py-2 row justify-content-between  border-bottom border-3 border-color-gold'>
+                    <Col xs={4} ><img className="footer-logo" src={logo} alt='Logo' /></Col>
+                    <Col xs={6} className='my-auto'><Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-warning">Search</Button>
+                    </Form>
+                    </Col>
+                </Row>
+                <Row lg={5} xs={2} className='py-2'>
                     <Col lg={true}>
                         <Col >EXPLORE</Col>
                         <Col >Swagatam</Col>
