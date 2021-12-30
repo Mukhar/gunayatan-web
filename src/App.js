56010bf81c3bb262e97c2c8a3294bf8967f27751
Mukhar Jain <Mukhar@users.noreply.github.com>
2021-12-30 19:19:30 +0530

    Added class addition and removal functionality on scroll

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/common.css"
-
 import RouteMain from './Route';
+import { useEffect } from 'react';
 function App() {
+  var nav = document.getElementById("navbar");
+
+  useEffect(() => {
+    window.onscroll = () => {
+      // console.log("hey scrolling:", window.scrollY);
+      if (window.scrollY > 100 && !nav.classList.contains("shrink")) {
+        nav.classList.add("shrink");
+        console.log("shrink added ");
+      } else if (window.scrollY == 0) {
+        if (nav.classList.contains("shrink"))
+          nav.classList.remove("shrink");
+        console.log("Scroll removed ");
+      }
+    }
+
+  }, []);
   return (<>
     <RouteMain />
   </>
