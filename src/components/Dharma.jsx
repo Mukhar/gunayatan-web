f266c36f9be78f10e60c517292e49d7bffde6aba
Mukhar Jain <Mukhar@users.noreply.github.com>
2022-01-01 16:01:34 +0530

    Added Quoted in dharma

--- src/components/Dharma.jsx
+++ src/components/Dharma.jsx
@@ -3,7 +3,11 @@ import Carousel from 'react-bootstrap/Carousel'
 import Col from 'react-bootstrap/esm/Col';
 import Container from 'react-bootstrap/esm/Container';
 import Row from 'react-bootstrap/esm/Row';
+import img1 from "./../assets/images/quote/quote1.jpg"
+import img2 from "./../assets/images/quote/quote2.jpeg"
+import img3 from "./../assets/images/quote/quote3.jpg"
 // import bg_section2 from './../assets/images/bg_section2.jpg'
+
 import "./../assets/css/dharma.css";
 export default function Dharma() {
     return (
@@ -17,6 +21,7 @@ export default function Dharma() {
                         <h3>Dharma</h3>
                         <Carousel variant='dark' prevIcon={false} nextIcon={false} className='dharma_carousel'>
                             <Carousel.Item interval={5000}>
+                                <img src={img1}></img>
                                 <p>My name is mukhar Jain</p>
                                 {/* <Carousel.Caption>
                                     <h3>First slide label</h3>
@@ -24,6 +29,7 @@ export default function Dharma() {
                                 </Carousel.Caption> */}
                             </Carousel.Item>
                             <Carousel.Item interval={5000}>
+                                <img src={img2}></img>
                                 <p>I am a devloper</p>
                                 {/* <Carousel.Caption>
                                     <h3>Second slide label</h3>
@@ -31,8 +37,8 @@ export default function Dharma() {
                                 </Carousel.Caption> */}
                             </Carousel.Item>
                             <Carousel.Item interval={5000} >
+                                <img src={img3}></img>
                                 <p>This is being developend by me </p>
-                                <p>Lorem ipsum dolor khadsfjhsdlkjfahdslkjfahdslkjfhadlskjfhsdlakjfhsdlfkjhsadlfkjdashlfkjhaldskjfhlj</p>
                                 {/* <Carousel.Caption>
                                     <h3>Third slide label</h3>
                                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
