826c0d3312f633d8fa75d55af14a8cac1bfaee18
Mukhar Jain <Mukhar@users.noreply.github.com>
2022-01-22 23:44:23 +0530

    Update Utils.jsx

--- src/components/Utils.jsx
+++ src/components/Utils.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { motion } from 'framer-motion';
 import "./../assets/css/common.css"
 import { useState } from 'react';
 import { useLayoutEffect } from 'react';
@@ -20,14 +19,15 @@ export default function ScrollToTop() {
             }
 
         }
-    }, []);
-    return (
-        <>
-            {scrollThresh && <motion.div whileTap={{ scale: 0.9 }} onClick={scrollup} >
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
-                </svg>
-            </motion.div >}
-        </>
+    }, [scrollThresh]);
+    return (<> {scrollThresh && <div className='scrollToTopButton ' onClick={scrollup} >
+        <div >
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" className="bi bi-arrow-up-square" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
+            </svg>
+        </div >
+    </div >}
+    </>
+
     )
 }
