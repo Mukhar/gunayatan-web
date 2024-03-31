3fd476f0d5b96f4eab73b5d439f79938749b3ce4
Mukhar Jain <Mukhar@users.noreply.github.com>
2024-03-31 16:59:51 +0530

    build fix

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react'
-import Carousel from 'react-bootstrap/Carousel'
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
 import "./../assets/css/carousel.css";
-import slide1 from "./../assets/images/ol/1.jpg";
-import slide2 from "./../assets/images/ol/2.jpg";
 import slide3 from "./../assets/images/carousel/theme7_1280x720.png";
 import slide5 from "./../assets/images/carousel/theme8_1280x720.png";
 import slide4 from "./../assets/images/carousel/theme9_1280x720.png";
@@ -10,40 +8,61 @@ import slide6 from "./../assets/images/carousel/banner2_1280x720.png";
 import slide7 from "./../assets/images/carousel/banner3_1280x720.png";
 import slide8 from "./../assets/images/carousel/banner4_1280x720.png";
 
-
-
 export default function CarouselBody() {
-    // eslint-disable-next-line
-    const [img1, setimg1] = useState([
-        // { img: slide1, label: "Gunayatan Label 1", caption: "Some Magnificent view of Gunayatan" },
-        // { img: slide2, label: "Gunayatan Label 2", caption: "Some Magnificent view of Gunayatan" },
-        { img: slide3, label: "Gunayatan Label 3", caption: "Some Magnificent view of Gunayatan" },
-        { img: slide4, label: "Gunayatan Label 4", caption: "Some Magnificent view of Gunayatan" },
-        { img: slide5, label: "Gunayatan Label 5", caption: "Some Magnificent view of Gunayatan" },
-        { img: slide6, label: "Gunayatan Label 5", caption: "Some Magnificent view of Gunayatan" },
-        { img: slide7, label: "Gunayatan Label 5", caption: "Some Magnificent view of Gunayatan" },
-        { img: slide8, label: "Gunayatan Label 5", caption: "Some Magnificent view of Gunayatan" },
-    ]);
-    return (
-        <div id='carousel-top'>
-            <Carousel fade className="TopCarousel">
-                {img1.map((element, i) => (
-                    <Carousel.Item interval={20000} key={`Carousel` + i} >
-                        <picture>
-                            <img
-                                className="d-block w-100"
-                                src={element.img}
-                                alt={element.label}
-                            />
-
-                        </picture>
-                        <Carousel.Caption>
-                            <h3>{element.label}</h3>
-                            <p>{element.caption}</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                ))
-                }</Carousel>
-        </div >
-    )
+  // eslint-disable-next-line
+  const [img1, setimg1] = useState([
+    // { img: slide1, label: "Gunayatan Label 1", caption: "Some Magnificent view of Gunayatan" },
+    // { img: slide2, label: "Gunayatan Label 2", caption: "Some Magnificent view of Gunayatan" },
+    {
+      img: slide3,
+      label: "Gunayatan Label 3",
+      caption: "Some Magnificent view of Gunayatan",
+    },
+    {
+      img: slide4,
+      label: "Gunayatan Label 4",
+      caption: "Some Magnificent view of Gunayatan",
+    },
+    {
+      img: slide5,
+      label: "Gunayatan Label 5",
+      caption: "Some Magnificent view of Gunayatan",
+    },
+    {
+      img: slide6,
+      label: "Gunayatan Label 5",
+      caption: "Some Magnificent view of Gunayatan",
+    },
+    {
+      img: slide7,
+      label: "Gunayatan Label 5",
+      caption: "Some Magnificent view of Gunayatan",
+    },
+    {
+      img: slide8,
+      label: "Gunayatan Label 5",
+      caption: "Some Magnificent view of Gunayatan",
+    },
+  ]);
+  return (
+    <div id="carousel-top">
+      <Carousel fade className="TopCarousel">
+        {img1.map((element, i) => (
+          <Carousel.Item interval={20000} key={`Carousel` + i}>
+            <picture>
+              <img
+                className="d-block w-100"
+                src={element.img}
+                alt={element.label}
+              />
+            </picture>
+            <Carousel.Caption>
+              <h3>{element.label}</h3>
+              <p>{element.caption}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,107 +1,192 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button'
-import Col from 'react-bootstrap/Col'
-import Container from 'react-bootstrap/Container'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Row from 'react-bootstrap/Row'
-import { Link } from 'react-router-dom'
-import "./../assets/css/footer.css"
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Row from "react-bootstrap/Row";
+import { Link } from "react-router-dom";
+import "./../assets/css/footer.css";
 // import logo from "./../assets/icons/logo with tag line-red.png"
-import bottom from "./../assets/icons/gunayatan_bottom.png"
-import logo from "./../assets/icons/logo with tag line-golden.png"
-import Facebook from './social/Facebook'
-import Instagram from './social/Instagram'
-import Twitter from './social/Twitter'
-import { HashLink } from 'react-router-hash-link';
-
-
-
+import logo from "./../assets/icons/logo with tag line-golden.png";
+import Facebook from "./social/Facebook";
+import Instagram from "./social/Instagram";
+import Twitter from "./social/Twitter";
+import { HashLink } from "react-router-hash-link";
 
 export default function Footer() {
-    return (<  >
+  return (
+    <>
+      <div className="sidebar">
+        <Facebook />
+        <Twitter />
+        <Instagram />{" "}
+      </div>
 
-        <div className='sidebar'>
-            <Facebook />
-            <Twitter />
-            <Instagram /> </div>
-
-        <Container id='footer' fluid className='footer py-2' >
-            <Container>
-                <Row className='py-2 row justify-content-between  border-bottom border-3 border-color-gold'>
-                    <Col xs={4}  ><img className="footer-logo" src={logo} alt='Logo' /></Col>
-                    {/* <Col xs={4} className='d-flex' style={{ justifyContent: "center" }}>
+      <Container id="footer" fluid className="footer py-2">
+        <Container>
+          <Row className="py-2 row justify-content-between  border-bottom border-3 border-color-gold">
+            <Col xs={4}>
+              <img className="footer-logo" src={logo} alt="Logo" />
+            </Col>
+            {/* <Col xs={4} className='d-flex' style={{ justifyContent: "center" }}>
                         <Facebook />
                         <Twitter />
                         <Instagram />
                     </Col> */}
-                    <Col xs={4} className='my-auto'><Form className="d-flex">
-                        <FormControl
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-warning">Search</Button>
-                    </Form>
-                    </Col>
-                </Row>
-                <Row lg={5} xs={2} className='py-2'>
-                    <Col lg={true}>
-                        <Col ><Link to="explore">EXPLORE</Link></Col>
-                        <Col > <Link to="/">Welcome</Link></Col>
-                        <Col > <Link to="/">Gunayatan Gyan Mandir</Link></Col>
-                        <Col > <Link to="/">Chaubisi Jinalay</Link></Col>
-                        <Col > <Link to="/">Sahasrakoot Jinalay</Link></Col >
-                        <Col > <Link to="/">Gunayatan Theme Park</Link></Col >
+            <Col xs={4} className="my-auto">
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-warning">Search</Button>
+              </Form>
+            </Col>
+          </Row>
+          <Row lg={5} xs={2} className="py-2">
+            <Col lg={true}>
+              <Col>
+                <Link to="explore">EXPLORE</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Welcome</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Gunayatan Gyan Mandir</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Chaubisi Jinalay</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Sahasrakoot Jinalay</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Gunayatan Theme Park</Link>
+              </Col>
+            </Col>
+            <Col lg={true}>
+              <Col>
+                <Link to="visit"> VISIT</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="visit#howtoreach">How to Reach</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Facilities</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Accomodation</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">What to See</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Tickets</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Security</Link>
+              </Col>
+            </Col>
+            <Col lg={true}>
+              <Col>WHATS NEW</Col>
+              <Col>
+                {" "}
+                <Link to="/">Progress </Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Views and Comments</Link>
+              </Col>
 
-                    </Col>
-                    <Col lg={true} >
-                        <Col ><Link to="visit"> VISIT</Link></Col >
-                        <Col > <Link to="visit#howtoreach">How to Reach</Link></Col >
-                        <Col > <Link to="/">Facilities</Link></Col >
-                        <Col > <Link to="/">Accomodation</Link></Col >
-                        <Col > <Link to="/">What to See</Link></Col >
-                        <Col > <Link to="/">Tickets</Link></Col >
-                        <Col > <Link to="/">Security</Link></Col >
-                    </Col>
-                    <Col lg={true}>
-                        <Col >WHATS NEW</Col>
-                        <Col > <Link to="/">Progress </Link></Col >
-                        <Col > <Link to="/">Views and Comments</Link></Col >
-
-                        <Col > <Link to="/">New Members</Link></Col >
-                        <Col > <Link to="/">Updates</Link></Col >
-
-
-                    </Col>
-                    <Col lg={true}>
-                        <Col ><Link to="aboutUs"> ABOUT US</Link></Col>
-                        <Col > <Link to="aboutUs#blessings">Blessings and Inspirer</Link></Col >
-                        <Col > <HashLink to="/aboutUs#Organisation">Organisation</HashLink></Col >
-                        <Col > <HashLink to="/prabandh" >Managing Comittee</HashLink></Col>
-                        <Col > <Link to="/">Gunayatan Journey </Link></Col >
-                        <Col > <HashLink to="/aboutUs#gunayatanParivar"> Gunayatan Parivar</HashLink></Col >
-                        <Col > <HashLink to="/aboutUs#MembersLogin" >Members Login</HashLink></Col >
-                        <Col > <HashLink to="/aboutUs#ContactUs" >Contact Us</HashLink></Col >
-                    </Col>
-                    <Col lg={true}>
-                        <Col >LINKS</Col>
-                        <Col ><a href="www.munipramansagar.net">Muni Pramansagar Ji</a></Col>
-                        <Col ><a href="google.com">Jain Pathshala</a></Col>
-                        <Col ><a href="www.google.co.in/search?&q=bhavna%20yog">Bhavna Yog</a></Col>
-                        <Col ><a href="www.google.com">Sevayatan</a></Col>
-                        <Col lg={true}>
-                            <Col>DOWNLOADS</Col>
-                            <Col> <Link to="/">Visitor Guide</Link></Col>
-                            <Col> <Link to="/">Photo Gallery</Link></Col>
-
-                        </Col>
-                    </Col>
-                </Row>
-                <Row className="justify-content-center">  © www.gunaytan.com</Row>
-            </Container>
-
+              <Col>
+                {" "}
+                <Link to="/">New Members</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Updates</Link>
+              </Col>
+            </Col>
+            <Col lg={true}>
+              <Col>
+                <Link to="aboutUs"> ABOUT US</Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="aboutUs#blessings">Blessings and Inspirer</Link>
+              </Col>
+              <Col>
+                {" "}
+                <HashLink to="/aboutUs#Organisation">Organisation</HashLink>
+              </Col>
+              <Col>
+                {" "}
+                <HashLink to="/prabandh">Managing Comittee</HashLink>
+              </Col>
+              <Col>
+                {" "}
+                <Link to="/">Gunayatan Journey </Link>
+              </Col>
+              <Col>
+                {" "}
+                <HashLink to="/aboutUs#gunayatanParivar">
+                  {" "}
+                  Gunayatan Parivar
+                </HashLink>
+              </Col>
+              <Col>
+                {" "}
+                <HashLink to="/aboutUs#MembersLogin">Members Login</HashLink>
+              </Col>
+              <Col>
+                {" "}
+                <HashLink to="/aboutUs#ContactUs">Contact Us</HashLink>
+              </Col>
+            </Col>
+            <Col lg={true}>
+              <Col>LINKS</Col>
+              <Col>
+                <a href="www.munipramansagar.net">Muni Pramansagar Ji</a>
+              </Col>
+              <Col>
+                <a href="google.com">Jain Pathshala</a>
+              </Col>
+              <Col>
+                <a href="www.google.co.in/search?&q=bhavna%20yog">Bhavna Yog</a>
+              </Col>
+              <Col>
+                <a href="www.google.com">Sevayatan</a>
+              </Col>
+              <Col lg={true}>
+                <Col>DOWNLOADS</Col>
+                <Col>
+                  {" "}
+                  <Link to="/">Visitor Guide</Link>
+                </Col>
+                <Col>
+                  {" "}
+                  <Link to="/">Photo Gallery</Link>
+                </Col>
+              </Col>
+            </Col>
+          </Row>
+          <Row className="justify-content-center"> © www.gunaytan.com</Row>
         </Container>
-    </>)
+      </Container>
+    </>
+  );
 }
